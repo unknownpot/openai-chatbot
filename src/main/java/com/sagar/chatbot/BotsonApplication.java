9be61a371df12ff233b265df24688eceb8347ac3
Sagar Ranjan <sagar.ranjan@amdocs.com>
2025-06-30 16:42:04 +0530

    hello

---
+++ src/main/java/com/sagar/chatbot/BotsonApplication.java
@@ -0,0 +1,13 @@
+package com.sagar.chatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotsonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sagar/chatbot/BotsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sagar.chatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
